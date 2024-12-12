e792f6550df1c40110d7d9bd48e1d53467db89de
Piyush Apkaje <apkajepiyush@gmail.com>
2024-12-12 12:03:12 +0530

    Quality Updates.

--- app/src/main/java/com/nextbigthing/thepunchline/MainApplication.kt
+++ app/src/main/java/com/nextbigthing/thepunchline/MainApplication.kt
@@ -1,7 +1,14 @@
 package com.nextbigthing.thepunchline
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this)
+    }
+}
