5bf9b60f49bb7766a1eb6e782275d27d9bee6831
Piyush Apkaje <apkajepiyush@gmail.com>
2025-01-20 12:18:24 +0530

    Version 1.7 Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.nextbigthing.thepunchline"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
